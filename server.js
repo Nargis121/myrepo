c0fcbeb83749a1d51231a1021be4190afda33c7f
Nargis Mehdi <143582690+Nargis121@users.noreply.github.com>
2024-11-21 01:44:26 -0800

    Add files via upload
    food app

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectdb=require("./confg/db");
 
 
-//env config
+
+//dot env configuration 
 dotenv.config();
-//routes import
-const userRoutes = require('./routes/userRoutes');
-const blogRoutes = require('./routes/blogRoutes');
+//DB connection 
+connectdb();
 
-//mongodb connection
-connectDB();
-//rest objects 
-const app =express();
 
-//middlewares
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
 
-//routes
-app.use('/api/v1/user' ,userRoutes);
-app.use('/api/v1/blog' , blogRoutes);
+
+//rest object
+const app = express()
+//midleware
+app.use(cors());
+//data access
+app.use(express.json());
+app.use(morgan('dev'));
+
+//route
+// url http://localhost:800
+app.use('/api/v1/test', require('./routes/testroutes'));
+
+
+
+app.get('/',(req,res)=>{
+    return res.status(200).send("<h1>Welcome to food web app</h1>")
+}) 
 
 //port
-const PORT = process.env.PORT || 8080
-//listen
-app.listen(PORT,()=>{
-    console.log(`Server running on ${ process.env.DEV_MODE} port no ${PORT}`);
+const port = process.env.port || 5000;
+
+
+//LISTEN
+app.listen(port,()=>{
+    console.log(`server running on ${port}`);
 });
